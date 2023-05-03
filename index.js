9c3f3545280ed3396cec9c09b3c543a9b1010176
Munieru <20086673+munierujp@users.noreply.github.com>
2023-05-03 17:28:16 +0900

    Fix errors

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const unicornRules = {
   'unicorn/no-useless-undefined': 'off',
   'unicorn/numeric-separators-style': 'off',
   'unicorn/prefer-module': 'off',
-  // TODO [engine:node@>=16]: Enable this when the minimum supported Node.js version becomes v16 or later.
-  'unicorn/prefer-node-protocol': 'off',
   'unicorn/prefer-query-selector': 'off',
   'unicorn/prefer-spread': 'off',
   // TODO [engine:node@>=18]: Enable this when the minimum supported Node.js version becomes v18 or later.
